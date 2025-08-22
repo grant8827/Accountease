997e7b8432f25c7d89726434a2c69f2a7700b66d
Gregory Grant <grant8827@yahoo.com>
2025-08-22 14:49:35 -0400

    Add healthcheck.railway.app to ALLOWED_HOSTS to fix Railway health checks

--- accounts_easy_project/settings.py
+++ accounts_easy_project/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY', default='django-insecure-^c^t^g*a^m#v1g6s3t@5a^j!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['accountease-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['accountease-production.up.railway.app', 'healthcheck.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
